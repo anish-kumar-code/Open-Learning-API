a6458a44abfd0fe4e97766b076e2756e588f0e36
Anish kumar <anishkr2842003@gmail.com>
2025-04-26 18:27:49 +0530

    populate category

--- src/controllers/admin/post/all.js
+++ src/controllers/admin/post/all.js
@@ -3,7 +3,7 @@ import postModel from "../../../models/postModel.js"
 const allPost = async (req, res) => {
 
     try {
-        const allPosts = await postModel.find();
+        const allPosts = await postModel.find().populate("category");
         return res.status(200).json({ statusCode: 200, message: "Post list", length: allPosts.length, allPosts });
 
     } catch (error) {
